a7815940dab9485c1550ff8c6dc4177538843e56
iamlucasgomes <lucas.devjs@gmail.com>
2022-08-31 02:19:58 -0300

    UPDATE: requeriment 02 implemented

--- src/App.js
+++ src/App.js
@@ -7,9 +7,40 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import { createUser } from './services/userAPI';
 
 class App extends React.Component {
+  state = {
+    inputName: '',
+    isButtonDisabled: true,
+    removeLoader: false,
+    redirect: false,
+  };
+
+  checkInputNameField = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => {
+      this.setState({
+        isButtonDisabled: (value.length < Number('3')),
+      });
+    });
+  };
+
+  handleClick = async () => {
+    const { inputName } = this.state;
+    this.setState(() => ({
+      removeLoader: true,
+    }));
+    await createUser({ name: inputName });
+    this.setState(() => ({
+      redirect: true,
+    }));
+  };
+
   render() {
+    const { isButtonDisabled, removeLoader, redirect } = this.state;
     return (
       <BrowserRouter>
         <Switch>
@@ -18,7 +49,17 @@ class App extends React.Component {
           <Route path="/favorites" component={ Favorites } />
           <Route exact path="/profile" component={ Profile } />
           <Route exact path="/profile/edit" component={ ProfileEdit } />
-          <Route exact path="/" component={ Login } />
+          <Route
+            exact
+            path="/"
+            render={ () => (<Login
+              checkInput={ this.checkInputNameField }
+              isButtonDisabled={ isButtonDisabled }
+              handleClick={ this.handleClick }
+              removeLoader={ removeLoader }
+              redirect={ redirect }
+            />) }
+          />
           <Route path="*" component={ NotFound } />
         </Switch>
       </BrowserRouter>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,51 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
 
 class Login extends Component {
   render() {
+    const {
+      checkInput,
+      handleClick,
+      isButtonDisabled,
+      removeLoader,
+      redirect,
+    } = this.props;
     return (
       <div data-testid="page-login">
-        <h1>Login page</h1>
+        <form>
+          <input
+            type="text"
+            name="inputName"
+            data-testid="login-name-input"
+            onChange={ checkInput }
+          />
+          <input
+            type="button"
+            value="Entrar"
+            data-testid="login-submit-button"
+            onClick={ handleClick }
+            disabled={ isButtonDisabled }
+          />
+          {
+            removeLoader && <Loading />
+          }
+          {
+            redirect && <Redirect to="/search" />
+          }
+        </form>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  checkInput: PropTypes.func.isRequired,
+  handleClick: PropTypes.func.isRequired,
+  isButtonDisabled: PropTypes.bool.isRequired,
+  removeLoader: PropTypes.bool.isRequired,
+  redirect: PropTypes.bool.isRequired,
+};
 
 export default Login;
