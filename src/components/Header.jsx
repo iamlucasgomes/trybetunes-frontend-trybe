7bc7b4872aa900a3a61da2f5a91451bdd62eeb90
iamlucasgomes <lucas.devjs@gmail.com>
2022-08-31 16:49:33 -0300

    UPDATE: requeriment 04 implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -24,6 +25,9 @@ class Header extends Component {
     return (
       <div data-testid="header-user-name">
         {loader && <Loading />}
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
         <header data-testid="header-component">{user}</header>
       </div>
     );
