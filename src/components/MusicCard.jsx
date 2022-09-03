a37ada720c8e10cd6e95f15d55f0a6a043fdcaba
iamlucasgomes <lucas.devjs@gmail.com>
2022-09-03 01:10:01 -0300

    UPDATE: requeriment 07 refactored

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MusicCard extends Component {
+  render() {
+    const { album } = this.props;
+    return (
+      <div>
+        {album
+          .filter((_, i) => i === 0)
+          .map(({ collectionName, artistName }) => (
+            <section key={ artistName }>
+              <h1 data-testid="album-name">{collectionName}</h1>
+              <h3 data-testid="artist-name">{artistName}</h3>
+            </section>
+          ))}
+        {album
+          .filter((_, i) => i > 0)
+          .map(({ trackName }) => <p key={ trackName }>{trackName}</p>)}
+        {album.filter((_, i) => i > 0).map(({ trackId, previewUrl }) => (
+          <div key={ trackId }>
+            <audio data-testid="audio-component" src={ previewUrl } controls>
+              <track kind="captions" />
+              O seu navegador não suporta o elemento
+              {' '}
+              {' '}
+              <code>audio</code>
+              .
+            </audio>
+            <label htmlFor="favorite">
+              Favorita
+              {' '}
+              {trackId}
+              <input
+                type="checkbox"
+                name="favorite"
+                data-testid={ `checkbox-music-${trackId}` }
+              />
+            </label>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+MusicCard.propTypes = {
+  album: PropTypes.objectOf.isRequired,
+};
+
+export default MusicCard;
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,15 +1,11 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import MusicCard from '../components/MusicCard';
 import getMusics from '../services/musicsAPI';
 
 class Album extends Component {
   state = {
     album: [],
-    artist: '',
-    collection: '',
-    musics: [],
-    preview: [],
-    trackId: [],
   };
 
   async componentDidMount() {
@@ -18,35 +14,16 @@ class Album extends Component {
     const { id } = params;
     this.setState({
       album: await getMusics(id),
-    }, () => {
-      const { album } = this.state;
-      this.setState({
-        artist: album.find((_, i) => i === 0).artistName,
-        collection: album.find((_, i) => i === 0).collectionName,
-        musics: album.filter((_, i) => i !== 0).map(({ trackName }) => trackName),
-        preview: album.filter((_, i) => i !== 0).map(({ previewUrl }) => previewUrl),
-        trackId: album.filter((_, i) => i !== 0).map(({ trackId }) => trackId),
-      });
     });
   }
 
   render() {
-    const { artist, collection, musics, preview, trackId } = this.state;
+    const { album } = this.state;
     return (
       <div data-testid="page-album">
-        <h1 data-testid="album-name">{collection}</h1>
-        <h3 data-testid="artist-name">{artist}</h3>
-        {musics.map((_, i) => <p key={ musics[i] }>{musics[i]}</p>)}
-        {preview.map(() => (
-          <audio key={ trackId } data-testid="audio-component" src={ preview } controls>
-            <track kind="captions" />
-            O seu navegador não suporta o elemento
-            {' '}
-            {' '}
-            <code>audio</code>
-            .
-          </audio>
-        ))}
+        <MusicCard
+          album={ album }
+        />
       </div>
     );
   }
